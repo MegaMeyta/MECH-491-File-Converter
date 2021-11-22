ee5880a97ed3a729f949604d8f3e97125e06e025
Joshua Metz <joshuametz033@gmail.com>
2021-11-22 14:54:49 -0800

    added and defined getAttributes and removeSpaces functions

--- APT Source.cpp
+++ APT Source.cpp
@@ -3,3 +3,144 @@
 #include <fstream>
 #include <vector>
 #include <map>
+
+
+void apt::CALSUB(FILE& output, vector<string> attributes) {
+	if (attributes[3] == "CLDATA") {
+
+	}
+
+	if (attributes[3] == "SYSTEM") {
+
+	}
+
+	if (attributes[3] == "INCLUD") {
+
+	}
+
+}
+
+void apt::COOLNT(FILE& output, vector<string> attributes) {
+
+}
+
+void apt::CUTCOM(FILE& output, vector<string> attributes) {
+	if (attributes[2] == "ON") {
+		if (attributes[3] == "LENGTH") {
+
+		}
+		if (attributes[3] == "XCOORD") {
+
+		}
+		if (attributes[3] == "YCOORD") {
+
+		}
+		if (attributes[3] == "ZCOORD") {
+
+		}
+		if (attributes[3] == "ADJUST") {
+
+		}
+	}
+	if (attributes[2] == "OFF") {
+		if (attributes[3] == "LENGTH") {
+
+		}
+		if (attributes[3] == "XCOORD") {
+
+		}
+		if (attributes[3] == "YCOORD") {
+
+		}
+		if (attributes[3] == "ZCOORD") {
+
+		}
+		if (attributes[3] == "ADJUST") {
+
+		}
+	}
+	if (attributes[2] == "LEFT") {
+		
+	}
+	if (attributes[2] == "RIGHT") {
+
+	}
+	if (attributes[2] == "CORNER") {
+
+	}
+	if (attributes[2] == "TRANSL") {
+
+	}
+	if (attributes[2] == "ROUND") {
+
+	}
+	if (attributes[2] == "NORMAL") {
+
+	}
+}
+
+void apt::CYCLE(FILE& output, vector<string> attributes);
+
+void apt::END(FILE& output, vector<string> attributes);
+
+void apt::FEDRAT(FILE& output, vector<string> attributes);
+
+void apt::LIMIT(FILE& output, vector<string> attributes);
+
+void apt::LOADTL(FILE& output, vector<string> attributes);
+
+void apt::MACHIN(FILE& output, vector<string> attributes);
+
+void apt::MOVETO(FILE& output, vector<string> attributes);
+
+void apt::MSYS(FILE& output, vector<string> attributes);
+
+void apt::OFSTNO(FILE& output, vector<string> attributes);
+
+void apt::OPNAME(FILE& output, vector<string> attributes);
+
+void apt::OPTYPE(FILE& output, vector<string> attributes);
+
+void apt::ORIGIN(FILE& output, vector<string> attributes);
+
+void apt::PARTNO(FILE& output, vector<string> attributes);
+
+void apt::PPRINT(FILE& output, vector<string> attributes);
+
+void apt::RAPID(FILE& output, vector<string> attributes);
+
+void apt::REWIND(FILE& output, vector<string> attributes);
+
+void apt::SPINDL(FILE& output, vector<string> attributes);
+
+void apt::STOP(FILE& output, vector<string> attributes);
+
+void apt::TLNAME(FILE& output, vector<string> attributes);
+
+void apt::TOOLNO(FILE& output, vector<string> attributes);
+
+void apt::GOTO(FILE& output, vector<string> attributes);
+
+void apt::MOVE(FILE& output, vector<string> attributes);
+
+void apt::TOLER(FILE& output, vector<string> attributes);
+
+void apt::INTOL(FILE& output, vector<string> attributes);
+
+void apt::OUTTOL(FILE& output, vector<string> attributes);
+
+void apt::CUTTER(FILE& output, vector<string> attributes);
+
+void apt::MULTAX(FILE& output, vector<string> attributes);
+
+void apt::UNITS(FILE& output, vector<string> attributes);
+
+void apt::TLAXIS(FILE& output, vector<string> attributes);
+
+void apt::FINI(FILE& output, vector<string> attributes);
+
+void apt::CONTRL(FILE& output, vector<string> attributes);
+
+void apt::SYN(FILE& output, vector<string> attributes);
+
+void apt::MOVARC(FILE& output, vector<string> attributes);
--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,56 @@
+#include <string>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
 #include "APT Source.h"
+
 using namespace std;
+using namespace apt;
+
+string removeSpaces(string line);
+
+vector<string> getAttributes(string line);
 
 int main() {
-	cout << "hello world";
+	string line = "My Things/ candy , computer, games, 20,  %terms , history";
+	vector<string> things = getAttributes(line);
+	for (int i = 0; i < things.size(); i++) {
+		cout << things[i] + "\n";
+	}
 	return 0;
+}
+
+vector<string> getAttributes(string line) {
+	vector<string> attributes;
+	string attribute;
+	int start = 0;
+	int end;
+	for (int i = 0; i < line.size(); i++) {
+		if (line[i] == '/') {
+			start = i + 1;
+		}
+		else if (line[i] == ',') {
+			attribute = line.substr(start, i - start);
+			attribute = removeSpaces(attribute);
+			attributes.push_back(attribute);
+			start = i + 1;
+		}
+		else if (i == line.size() - 1 ) {
+			attribute = line.substr(start, i + 1 - start);
+			attribute = removeSpaces(attribute);
+			attributes.push_back(attribute);
+		}
+	}
+	return attributes;
+}
+
+string removeSpaces(string line) {
+	string result;
+	for (int i = 0; i < line.size(); i++) {
+		if (line[i] != ' ') {
+			result.push_back(line[i]);
+		}
+	}
+	return result;
 }
